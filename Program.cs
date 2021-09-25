4dc0fbc6d2dd28034ab6cc7fae66257f07503563
Bruno de Almeida <brunoalm.tj@gmail.com>
2021-09-25 11:37:20 -0300

    Message Good Morning included

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ProjetoExemplo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good morning!");
         }
     }
 }
